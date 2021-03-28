9df544575266bfc41b14358e2d3465c8f5d6e261
NadraniYassine <75631217+NadraniYassine@users.noreply.github.com>
2021-03-28 07:05:01 +0200

    Update jeu.py

--- jeu.py
+++ jeu.py
@@ -1,85 +1,84 @@
-class Jeu:
+class Game:
     def __init__(self):
-        self.plateau=[[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0]]
-
+        self.board=[[0 for i in range(7)] for j in range(6)]
     #Exerci 2
     def afficher(self):
-        for i in range (len (self.plateau)):
-            ligne="|"
-            for j in range (len(self.plateau[0])):
-                if self.plateau[len (self.plateau)-1-i][j]==0:
-                    ligne+=" |"
-                if self.plateau[len (self.plateau)-1-i][j]==1:
-                    ligne+="X|"
-                if self.plateau[len (self.plateau)-1-i][j]==2:
-                    ligne+="O|"
-            print(ligne)
+        for i in range (len (self.board)):
+            line="|"
+            for j in range (len(self.board[0])):
+                if self.board[len (self.board)-1-i][j]==0:
+                    line+=" |"
+                if self.board[len (self.board)-1-i][j]==1:
+                    line+="X|"
+                if self.board[len (self.board)-1-i][j]==2:
+                    line+="O|"
+            print(line)
         print(" - - - - - - - ")
         print(" 1 2 3 4 5 6 7 ")
-    def combiendanscolonne(self,numcolonne):
+    def checkersInColmun(self,numColumn):
         i=0
-        for k in range (len(self.plateau)):
-            if self.plateau[k][numcolonne]!=0:
+        for k in range (len(self.board)):
+            if self.board[k][numColumn]!=0:
                 i+=1
         return(i)
-    def empiler(self,couleur,numcolonne):
-        nb_colonne=self.combiendanscolonne(numcolonne)
-        self.plateau[nb_colonne][numcolonne]=couleur
+    def empiler(self,colour,numColumn):
+        nb_column=self.checkersInColmun(numColumn)
+        self.board[nb_column][numColumn]=colour
         self.afficher()
-    def combien_dans_la_direction(self,t,i,j,delta_i, delta_j):
-#arguments: plateau, numéro de ligne, numéro de colonne,delta_i, delta_j
-        compteur=0 #nombre de pions identiques selon la directiondemandée
-        couleur=t[i][j]
-        while i+delta_i>=0 and i+delta_i<len(t) and j+delta_j>=0 and j+delta_j<len(t[i]) and t[i+delta_i][j+delta_j]==couleur:
-#tant que la case voisine est dans le plateau et est de lamême couleur
-            compteur+=1 #on comte le pion dans cette case voisine
+    def checkersInDirection(self,t,i,j,delta_i, delta_j):
+#arguments: board, numéro de line, numéro de column,delta_i, delta_j
+        counter=0 #nombre de pions identiques selon la directiondemandée
+        colour=t[i][j]
+        while i+delta_i>=0 and i+delta_i<len(t) and j+delta_j>=0 and j+delta_j<len(t[i]) and t[i+delta_i][j+delta_j]==colour:
+#tant que la case voisine est dans le board et est de lamême colour
+            counter+=1 #on comte le pion dans cette case voisine
 #on doit aussi décaler la case dans la directiondemandée pour préparer le prochain test (le voisin du voisin):
             i=i+delta_i
             j=j+delta_j
-        return compteur
+        return counter
 
-    def test_lignes(self,colonne,ligne):
-        nb1=self.combien_dans_la_direction(self.plateau,ligne,colonne,0,1)
-        nb2=self.combien_dans_la_direction(self.plateau,ligne,colonne,0,-1)
+    def test_lines(self,column,line):
+        nb1=self.checkersInDirection(self.board,line,column,0,1)
+        nb2=self.checkersInDirection(self.board,line,column,0,-1)
         if nb1+nb2+1==4:
-            print("Le joueur ",self.plateau[ligne][colonne]," gagne ! Bravo !")
+            print("The player ",self.board[line][column]," won! Bravo! ")
             return(True)
         return(False)
 
-    def test_colonne(self,colonne,ligne):
-        nb1=self.combien_dans_la_direction(self.plateau,ligne,colonne,1,0)
-        nb2=self.combien_dans_la_direction(self.plateau,ligne,colonne,-1,0)
+    def test_column(self,column,line):
+        nb1=self.checkersInDirection(self.board,line,column,1,0)
+        nb2=self.checkersInDirection(self.board,line,column,-1,0)
         if nb1+nb2+1==4:
-            print("Le joueur ",self.plateau[ligne][colonne]," gagne ! Bravo !")
+            print("The player ",self.board[line][column]," won! Bravo!")
             return(True)
         return(False)
 
-    def test_diagonale(self,colonne,ligne):
-        nb1=self.combien_dans_la_direction(self.plateau,ligne,colonne,1,1)
-        nb2=self.combien_dans_la_direction(self.plateau,ligne,colonne,-1,-1)
+    def test_diagonal(self,column,line):
+        nb1=self.checkersInDirection(self.board,line,column,1,1)
+        nb2=self.checkersInDirection(self.board,line,column,-1,-1)
         if nb1+nb2+1==4:
-            print("Le joueur ",self.plateau[ligne][colonne]," gagne ! Bravo !")
+            print("The player ",self.board[line][column]," won ! Bravo!")
             return(True)
-        nb1=self.combien_dans_la_direction(self.plateau,ligne,colonne,-1,1)
-        nb2=self.combien_dans_la_direction(self.plateau,ligne,colonne,1,-1)
+        nb1=self.checkersInDirection(self.board,line,column,-1,1)
+        nb2=self.checkersInDirection(self.board,line,column,1,-1)
         if nb1+nb2+1==4:
-            print("Le joueur ",self.plateau[ligne][colonne]," gagne ! Bravo !")
+            print("The player ",self.board[line][column]," won! Bravo!")
             return(True)
         return(False)
 
-    def test_tout(self,colonne_choisi):
+    def testAll(self,chosenColumn):
         k=0
-        while self.plateau[k][colonne_choisi-1]!=0 and k<5:
+        while self.board[k][chosenColumn-1]!=0 and k<5:
             k+=1
-        ligne=k-1
-        colonne=colonne_choisi-1
-        if self.test_lignes(colonne,ligne):
+        line=k-1
+        column=chosenColumn-1
+        if self.test_lines(column,line):
             return(True)
-        if self.test_colonne(colonne,ligne):
+        if self.test_column(column,line):
             return(True)
-        if self.test_diagonale(colonne,ligne):
+        if self.test_diagonal(column,line):
             return(True)
         return(False)
-MonJeu=Jeu()
-MonJeu.afficher()
-print(MonJeu.plateau)
+MyGame=Game()
+MyGame.afficher()
+print(MyGame.board)
